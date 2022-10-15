658814e8279e6cca2d3cc4b053bdede43d658f5a
Karimov.Renat <karrent32@yandex.ru>
2022-10-15 12:07:24 +0300

    мой первый коммит.

---
+++ converter.java
@@ -0,0 +1,16 @@
+public class converter(){
+
+/*    public static void example() {
+        int command = 0; // Попробуйте поменять значение команды на 0 
+        if (command == 1) {
+            System.out.println("Привет!");
+        } else if (command == 0) {
+            System.out.println("Выход");
+            return; // Оператор return стоит в конце блока кода ветвления
+            // При сommand == 0 метод будет сразу завершён
+        }
+        // Эта строка будет выведена, если метод не завершился (сommand == 1)
+        System.out.println("Напечатаем ещё одну строку");
+    } */
+
+}
